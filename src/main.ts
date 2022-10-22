1b1db1fd5212e7468f8b1c463e02d51a7ebe3167
@mamedov.a <mamedov.a@skbkontur.ru>
2022-10-22 20:39:12 +0500

    Add vuetify

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
 import { createApp } from 'vue';
 import App from './App.vue';
+import vuetify from './plugins/vuetify';
 import { router } from './router';
+import { loadFonts } from './plugins/webfontloader';
 
-createApp(App).use(router).mount('#app');
+loadFonts();
+
+createApp(App).use(vuetify).use(router).mount('#app');
